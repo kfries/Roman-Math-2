4b5f777a8953200d8ff8584b80834b2eec10947b
Kevin Fries <kevin@fries-biro.com>
2016-08-08 17:15:23 +0000

    Tested to_string with multi-digit values

--- test/RomanNumberMath.c
+++ test/RomanNumberMath.c
@@ -242,6 +242,11 @@ START_TEST(roman_numberal_to_string_single_digit) {
    ck_assert_str_eq("I", to_string(newRomanNumber("i")));
 } END_TEST
 
+/* Testing "to_string()" with multi-digit value */
+START_TEST(roman_numberal_to_string_multi_digit) {
+   ck_assert_str_eq("VI", to_string(newRomanNumber("vi")));
+} END_TEST
+
 Suite * roman_number_math_suite(void) {
    Suite *testSuite;
    TCase *tc_roman_digit, *tc_roman_number, *tc_debug;
@@ -285,6 +290,7 @@ Suite * roman_number_math_suite(void) {
 
    tcase_add_test(tc_debug, roman_numberal_to_string_function);
    tcase_add_test(tc_debug, roman_numberal_to_string_single_digit);
+   tcase_add_test(tc_debug, roman_numberal_to_string_multi_digit);
 
    suite_add_tcase(testSuite, tc_debug);
 
