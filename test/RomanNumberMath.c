8ee3d8ed17b9ebbfcf7f7fad1ac57b8b25de9360
Kevin Fries <kevin@fries-biro.com>
2016-08-11 16:07:34 +0000

    Sort Digit Test: sort resulting in non-standard values

--- test/RomanNumberMath.c
+++ test/RomanNumberMath.c
@@ -423,6 +423,15 @@ START_TEST(sort_test_resulting_in_non_standard_value) {
    ck_assert_str_eq("XVIIIII", to_string(rn));
 } END_TEST
 
+/**************** Sort Test: Sort Multiple Values Out-Of-Order ****************/
+START_TEST(sort_test_multiple_digits_out_of_order) {
+   RomanNumber rn = newRomanNumber("VIXVIIII");
+
+   rnSort(&rn);
+
+   ck_assert_str_eq("XVVIIIII", to_string(rn));
+} END_TEST
+
 Suite * roman_number_math_suite(void) {
    Suite *testSuite;
    TCase *tc_roman_digit, *tc_roman_number, *tc_debug, *tc_remove_sub, *tc_concat, *tc_sort;
@@ -503,6 +512,7 @@ Suite * roman_number_math_suite(void) {
    tcase_add_test(tc_sort, sort_test_already_sorted_values_return_themselves);
    tcase_add_test(tc_sort, sort_test_small_simple_value);
    tcase_add_test(tc_sort, sort_test_resulting_in_non_standard_value);
+   tcase_add_test(tc_sort, sort_test_multiple_digits_out_of_order);
 
    suite_add_tcase(testSuite, tc_sort);
 
